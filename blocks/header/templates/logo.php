dfa949241bd82d83b1d3462f729c6c088aac56c0
Ben Leonard <ben.leonard@creode.co.uk>
2024-07-16 10:52:32 +0100

    Added hyperlink to header logo block.

--- blocks/header/templates/logo.php
+++ blocks/header/templates/logo.php
@@ -9,9 +9,11 @@
 
 <div class="header__section header__section--logo">
 	<div class="header__logo-wrapper">
-		<img
-			src="<?php echo esc_url( apply_filters( 'header_logo_src', get_stylesheet_directory_uri() ) ); ?>/images/logo.svg"
-			alt="<?php echo esc_html( apply_filters( 'header_logo_alt', get_bloginfo( 'name' ) ) ); ?>"
-		/>
+		<a href="<?php echo esc_url( is_admin() ? '#' : apply_filters( 'header_logo_href', get_home_url() ) ); ?>">
+			<img
+				src="<?php echo esc_url( apply_filters( 'header_logo_src', get_stylesheet_directory_uri() ) ); ?>/images/logo.svg"
+				alt="<?php echo esc_html( apply_filters( 'header_logo_alt', get_bloginfo( 'name' ) ) ); ?>"
+			/>
+		</a>
 	</div>
 </div>
