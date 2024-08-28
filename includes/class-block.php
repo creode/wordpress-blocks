062c29c450f1c303294852b61e0ea6e9f3a3d6bf
Jamie Sykes <jaymeh@users.noreply.github.com>
2024-08-28 15:37:39 +0100

    Switch to using core block registration instead of ACF, also allow icons to be used.

--- includes/class-block.php
+++ includes/class-block.php
@@ -11,41 +11,47 @@ namespace CreodeBlocks;
  * Abstract class to extend for each block.
  */
 abstract class Block {
+	/**
+	 * The blocks icon from https://developer.wordpress.org/resource/dashicons/
+	 *
+	 * @var string
+	 */
+	protected $icon = 'block-default';
 
 	/**
 	 * Function for providing the block's name.
 	 *
 	 * @return string The block's name (must be hyphen separated).
 	 */
-	abstract protected function name();
+	abstract protected function name(): string;
 
 	/**
 	 * Function for providing the block's label to be used within the WordPress UI.
 	 *
 	 * @return string The block's label.
 	 */
-	abstract protected function label();
+	abstract protected function label(): string;
 
 	/**
 	 * Function for providing fields array.
 	 *
 	 * @return array An array of field definitions in ACF format. Please see: https://www.advancedcustomfields.com/resources/register-fields-via-php/.
 	 */
-	abstract protected function fields();
+	abstract protected function fields(): array;
 
 	/**
 	 * Function for providing a path to the render template.
 	 *
 	 * @return string The path to the render template.
 	 */
-	abstract protected function template();
+	abstract protected function template(): string;
 
 	/**
 	 * Function for providing the block's category.
 	 *
 	 * @return string The block's category.
 	 */
-	protected function category() {
+	protected function category(): string {
 		return 'common';
 	}
 
@@ -54,7 +60,7 @@ abstract class Block {
 	 *
 	 * @return ChildBlock[] Array of child blocks.
 	 */
-	protected function child_blocks() {
+	protected function child_blocks(): array {
 		return array();
 	}
 
@@ -66,35 +72,76 @@ abstract class Block {
 		$this->register_acf_fields();
 
 		foreach ( $this->child_blocks() as $child_block ) {
-			$this->register_child_block( $this->name(), $child_block, $this->name() );
+			$this->register_child_block( 'acf/' . $this->name(), $child_block, $this->name() );
 		}
 	}
 
 	/**
 	 * Function to register the block with ACF.
 	 */
-	protected function register_acf_block() {
-		if ( ! function_exists( 'acf_register_block_type' ) ) {
-			return;
-		}
-
-		acf_register_block_type(
+	protected function register_acf_block(): void {
+		$this->register_block_type(
 			array(
-				'name'            => $this->name(),
-				'title'           => $this->label(),
-				'category'        => $this->category(),
-				'render_template' => $this->template(),
-				'supports'        => array(
-					'jsx' => true,
-				),
+				'name'       => 'acf/' . $this->name(),
+				'title'      => $this->label(),
+				'category'   => $this->category(),
+				'icon'       => $this->icon,
+				'render'     => $this->template(),
+				'textdomain' => 'wordpress-blocks',
 			)
 		);
 	}
 
 	/**
+	 * Registers the block type using the provided block data.
+	 *
+	 * @param array $block_data 
+	 *
+	 * @return void
+	 */
+	protected function register_block_type( array $block_data ): void {
+		// Check if the cache folder exists.
+		$cache_folder = WP_CONTENT_DIR . '/cache/wp-blocks/';
+		$block_folder = $cache_folder . str_replace( '/', '-', $block_data['name'] );
+		$cache_file = $block_folder . '/block.json';
+		if ( file_exists( $cache_file ) ) {
+			register_block_type( $block_folder );
+			return;
+		}
+
+		// Create cache folder.
+		if ( ! file_exists( $cache_folder ) ) {
+			mkdir( $cache_folder, 0755, true );
+		}
+
+		// Create block folder.
+		if ( ! file_exists( $block_folder ) ) {
+			mkdir( $block_folder, 0755, true );
+		}
+
+		// Attach block schema to block data.
+		$block_data['$schema']    = 'https://schemas.wp.org/trunk/block.json';
+		$block_data['apiVersion'] = 3;
+
+		// Setup ACF Configuration for block.
+		$block_data['acf'] = array(
+			'mode'           => 'preview',
+			'renderTemplate' => $block_data['render'],
+		);
+
+		// Remove render key from block data as we no longer need it.
+		unset( $block_data['render'] );
+
+		// Save the block contents to cache file.
+		file_put_contents( $cache_file, json_encode( $block_data, JSON_UNESCAPED_SLASHES ) );
+
+		register_block_type( $block_folder );
+	}
+
+	/**
 	 * Function to register the block's fields with ACF.
 	 */
-	protected function register_acf_fields() {
+	protected function register_acf_fields(): void {
 		if ( ! function_exists( 'acf_add_local_field_group' ) ) {
 			return;
 		}
@@ -123,24 +170,22 @@ abstract class Block {
 	 * @param string     $parent_block The parent block name.
 	 * @param ChildBlock $child_block The child block to register.
 	 */
-	protected function register_child_block( string $parent_block, ChildBlock $child_block ) {
+	protected function register_child_block( string $parent_block, ChildBlock $child_block ): void {
 		if ( ! function_exists( 'acf_register_block_type' ) ) {
 			return;
 		}
 
-		// Register ACF block.
-		acf_register_block_type(
+		$this->register_block_type(
 			array(
-				'name'            => $parent_block . '-' . $child_block->name,
-				'title'           => $child_block->label,
-				'render_template' => $child_block->template,
-				'category'        => $this->category(),
-				'parent'          => array(
-					'acf/' . $parent_block,
+				'name'       => $parent_block . '-' . $child_block->name,
+				'title'      => $child_block->label,
+				'render'     => $child_block->template,
+				'category'   => $this->category(),
+				'icon'       => $child_block->icon,
+				'parent'     => array(
+					$parent_block,
 				),
-				'supports'        => array(
-					'jsx' => true,
-				),
+				'textdomain' => 'wordpress-blocks',
 			)
 		);
 
@@ -148,10 +193,10 @@ abstract class Block {
 		add_filter(
 			'creode_blocks_parent_child_relationship',
 			function ( array $blocks ) use ( $parent_block, $child_block ) {
-				if ( empty( $blocks[ 'acf/' . $parent_block ] ) ) {
-					$blocks[ 'acf/' . $parent_block ] = array();
+				if ( empty( $blocks[ $parent_block ] ) ) {
+					$blocks[ $parent_block ] = array();
 				}
-				array_push( $blocks[ 'acf/' . $parent_block ], 'acf/' . $parent_block . '-' . $child_block->name );
+				array_push( $blocks[ $parent_block ], $parent_block . '-' . $child_block->name );
 
 				return $blocks;
 			}
@@ -169,7 +214,7 @@ abstract class Block {
 							array(
 								'param'    => 'block',
 								'operator' => '==',
-								'value'    => 'acf/' . $parent_block . '-' . $child_block->name,
+								'value'    => $parent_block . '-' . $child_block->name,
 							),
 						),
 					),
