679f53c16485440b9f33e40afcfeb2a0f044b4bc
Ben Leonard <ben.leonard@creode.co.uk>
2024-10-24 14:05:23 +0100

    Added missing dock block.

--- includes/class-helpers.php
+++ includes/class-helpers.php
@@ -24,6 +24,12 @@ class Helpers {
 		return isset( $blocks[ $name ] ) ? $blocks[ $name ] : null;
 	}
 
+	/**
+	 * Sets the ACF block mode.
+	 *
+	 * @param string $block_name The full block name including vendor prefix.
+	 * @param string $mode The ACF block mode.
+	 */
 	public static function set_acf_block_mode( string $block_name, string $mode = 'preview' ) {
 		add_filter(
 			$block_name . '_acf_block_mode',
