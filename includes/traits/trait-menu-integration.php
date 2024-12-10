ac677a9e17b58a83089c1f463bd3263f634cb2f6
Ben Leonard <ben.leonard@creode.co.uk>
2024-12-10 16:39:28 +0000

    Refactor of menu integration trait. Added an additional public function and additional arguments for menu rendering.

--- includes/traits/trait-menu-integration.php
+++ includes/traits/trait-menu-integration.php
@@ -32,17 +32,10 @@ trait Trait_Menu_Integration {
 	 * Renders a menu by menu location.
 	 *
 	 * @param string $location The menu location.
+	 * @param array  $args (optional) An array of arguments for rendering the menu. Please see https://developer.wordpress.org/reference/functions/wp_nav_menu/ for a full list of arguments.
 	 */
-	public function render_menu_by_location( string $location ): void {
-		$menu_id = null;
-
-		foreach ( get_nav_menu_locations() as $loop_location => $loop_menu_id ) {
-			if ( $loop_location !== $location ) {
-				continue;
-			}
-
-			$menu_id = $loop_menu_id;
-		}
+	public function render_menu_by_location( string $location, array $args = array() ): void {
+		$menu_id = $this->get_menu_id_by_location( $location );
 
 		if ( ! $menu_id ) {
 			echo 'No menu found.';
@@ -50,10 +43,31 @@ trait Trait_Menu_Integration {
 		}
 
 		wp_nav_menu(
-			array(
-				'container' => '',
-				'menu'      => $menu_id,
+			array_merge(
+				array(
+					'container' => '',
+					'menu'      => $menu_id,
+				),
+				$args
 			)
 		);
+	}
+
+	/**
+	 * Returns the ID of a menu assigned to a specified location.
+	 *
+	 * @param string $location The menu location.
+	 * @return int|null The menu id. Null If menu cannot be found.
+	 */
+	public function get_menu_id_by_location( string $location ): int|null {
+		foreach ( get_nav_menu_locations() as $loop_location => $loop_menu_id ) {
+			if ( $loop_location !== $location ) {
+				continue;
+			}
+
+			return $loop_menu_id;
+		}
+
+		return null;
 	}
 }
