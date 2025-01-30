1c5b6ec3bc47da8a91a1acdf61b416fa336de338
Jamie Sykes <jaymeh@users.noreply.github.com>
2025-01-30 15:29:08 +0000

    Adds docblock to default results template to aid in amendments and autocompletion.

--- blocks/search-and-filter/templates/defaults/results.php
+++ blocks/search-and-filter/templates/defaults/results.php
@@ -6,6 +6,11 @@
  */
 
 if ( class_exists( 'Creode_Blocks\Helpers' ) ) {
+	/**
+	 * Search and Filter block class.
+	 *
+	 * @var Search_And_Filter_Block $block
+	 */
 	$block = Creode_Blocks\Helpers::get_block_by_name( 'search-and-filter' );
 	$block->render_results_list( $args, $query );
 }
