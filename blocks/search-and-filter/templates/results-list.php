86d15efc18c01787a818147db3bdab5ae6397a05
Jamie Sykes <jaymeh@users.noreply.github.com>
2025-02-10 10:43:14 +0000

    Adjust the Search and filter no results filter to give developers more control over the markup.

--- blocks/search-and-filter/templates/results-list.php
+++ blocks/search-and-filter/templates/results-list.php
@@ -17,7 +17,7 @@
 		<?php endwhile; ?>
 	</div>
 <?php else : ?>
-	<p><?php echo esc_html( apply_filters( 'creode_blocks_search_and_filter_results_list_empty_message', 'No Results found.', $args, $query ) ); ?></p>
+	<?php echo wp_kses_post( apply_filters( 'creode_blocks_search_and_filter_results_list_empty_message', '<p>No Results found.</p>', $args, $query ) ); ?>
 <?php endif; ?>
 
 <?php wp_reset_postdata(); ?>
