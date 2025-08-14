e1a47728920eed80ed13a3809ee009d00cd7c0bf
Ben Leonard <ben.leonard@creode.co.uk>
2025-08-14 12:17:11 +0100

    Added inclusion of legacy header block.

--- blocks/all.php
+++ blocks/all.php
@@ -42,6 +42,6 @@ add_action(
 );
 
 // Load optional blocks.
-require_once plugin_dir_path( __FILE__ ) . 'header/class-header-block.php';
+require_once plugin_dir_path( __FILE__ ) . 'legacy/header/class-header-block.php';
 require_once plugin_dir_path( __FILE__ ) . 'post-listing/class-post-listing-block.php';
 require_once plugin_dir_path( __FILE__ ) . 'search-and-filter/class-search-and-filter-block.php';
