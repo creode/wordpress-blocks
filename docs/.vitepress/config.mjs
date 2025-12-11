623bf193d28f3f8c190b2d62774105bbe55d6554
Jamie Sykes <jaymeh@users.noreply.github.com>
2025-12-11 11:32:17 +0000

    Add more examples, more details and reorder the migration guides in the sidebar.

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -22,9 +22,9 @@ export default defineConfig({
           { text: 'Quick Start', link: '/quick-start' },
           { text: 'Upgrading', link: '/upgrading', items: [
             { text: 'Introduction', link: '/upgrading/index.md' },
-            { text: '2.x to 3.x', link: '/upgrading/2.x-3.x.md' },
-            { text: '1.x to 2.x', link: '/upgrading/1.x-2.x.md' },
             { text: '0.x to 1.x', link: '/upgrading/0.x-1.x.md' },
+            { text: '1.x to 2.x', link: '/upgrading/1.x-2.x.md' },
+            { text: '2.x to 3.x', link: '/upgrading/2.x-3.x.md' },
             { text: 'Rector', link: '/upgrading/rector.md' },
           ]},
           { text: 'Changelog', link: '/changelog' },
